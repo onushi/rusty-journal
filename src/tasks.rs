6cdccbf0c424fd66296b4831d6526832e082d03f
onushi <4374999+onushi@users.noreply.github.com>
2022-03-12 00:58:45 +0900

    add_task, complete_task

--- src/tasks.rs
+++ src/tasks.rs
@@ -16,3 +16,54 @@ impl Task {
     Task { text, created_at }
   }
 }
+
+use std::fs::{File, OpenOptions};
+use std::path::PathBuf;
+use std::io::{Result, Seek, SeekFrom};
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+  let mut file = OpenOptions::new()
+    .read(true)
+    .write(true)
+    .create(true)
+    .open(journal_path)?;
+
+  let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+    Ok(tasks) => tasks,
+    Err(e) if e.is_eof() => Vec::new(),
+    Err(e) => Err(e)?,
+  };
+
+  file.seek(SeekFrom::Start(0))?;
+
+  tasks.push(task);
+  serde_json::to_writer(file, &tasks)?;
+
+  Ok(())
+}
+
+use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};
+
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+  let file = OpenOptions::new()
+    .read(true)
+    .write(true)
+    .open(journal_path)?;
+
+  let tasks = match serde_json::from_reader(file) {
+    Ok(tasks) => tasks,
+    Err(e) if e.is_eof() => Vec::new(),
+    Err(e) => Err(e)?,
+  };
+
+  if task_position == 0 || task_position > tasks.len() {
+    return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
+  }
+  tasks.remove(task_position - 1);
+
+  file.seek(SeekFrom::Start(0))?;
+  file.set_len(0)?;
+
+  serde_json::to_writer(file, &tasks)?;
+  Ok(())
+}
