5bdac40a6cdb679b4051a6376b4b62e9c4075d07
onushi <4374999+onushi@users.noreply.github.com>
2022-06-10 12:17:49 +0900

    タスク一覧表示を実装し、メイン解禁

--- src/tasks.rs
+++ src/tasks.rs
@@ -22,18 +22,18 @@ use std::path::PathBuf;
 use std::io::{Result, Seek, SeekFrom};
 
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
-  let mut file = OpenOptions::new()
+  let file = OpenOptions::new()
     .read(true)
     .write(true)
     .create(true)
     .open(journal_path)?;
-  let mut tasks = collect_tasks(file);
+  let mut tasks = collect_tasks(&file)?;
   tasks.push(task);
   serde_json::to_writer(file, &tasks)?;
   Ok(())
 }
 
-use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};
+use std::io::{Error, ErrorKind};
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
   let file = OpenOptions::new()
@@ -58,8 +58,34 @@ fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
   let tasks = match serde_json::from_reader(file) {
     Ok(tasks) => tasks,
     Err(e) if e.is_eof() => Vec::new(),
-    Err(e) => Err(e),
+    Err(e) => Err(e)?,
   };
   file.seek(SeekFrom::Start(0))?;
   Ok(tasks)
+}
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+  let file = OpenOptions::new().read(true).open(journal_path)?;
+  let tasks = collect_tasks(&file)?;
+
+  if tasks.is_empty() {
+    println!("Task list is empty!")
+  } else {
+    let mut order: u32 = 1;
+    for task in tasks {
+      println!("{}: {}", order, task);
+      order += 1;
+    }
+  }
+
+  Ok(())
+}
+
+use std::fmt;
+
+impl fmt::Display for Task {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+    write!(f, "{:<50} [{}]", self.text, created_at)
+  }
 }
