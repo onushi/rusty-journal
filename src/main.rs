8a84f6cb5fec2868594498475406760cb0a1d88a
onushi <4374999+onushi@users.noreply.github.com>
2022-06-10 12:47:07 +0900

    anyhowでエラーメッセージをフレンドリーに

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-mod cli;
-mod tasks;
 use structopt::StructOpt;
 use std::path::PathBuf;
+use anyhow::anyhow;
+mod cli;
+mod tasks;
 
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
@@ -13,7 +14,7 @@ fn find_default_journal_file() -> Option<PathBuf> {
     })
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let CommandLineArgs {
         action,
         journal_file,
@@ -21,11 +22,12 @@ fn main() {
 
     let journal_file = journal_file
         .or_else(find_default_journal_file)
-        .expect("Failed to find journal file");
+        .ok_or(anyhow!("Failed to find journal file."))?;
 
     match action {
         Add { text } => tasks::add_task(journal_file, Task::new(text)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }.expect("Failed to perform action")
+    }?;
+    Ok(())
 }
